a7415b01a4ed7783e8770758c2b422f1331f88ce
Lorano98 <evalangstein@freenet.de>
2022-08-21 13:31:57 +0200

    add leaflet

--- public/javascripts/gebirgeAnlegen.js
+++ public/javascripts/gebirgeAnlegen.js
@@ -1,57 +1,54 @@
-var express = require('express');
-var router = express.Router();
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+var mark = null;
+var coords = null;
+var nameInput = document.getElementById("name");
+var hoeheInput = document.getElementById("hoehe");
+var urlInput = document.getElementById("url");
+var xInput = document.getElementById("x");
+var yInput = document.getElementById("y");
 
-const url = 'mongodb://localhost:27017'; // connection URL
-const client = new MongoClient(url); // mongodb client
-const dbName = 'mydb'; // database name
-const collectionName = 'pois'; // collection nam
+// Karte mit Zentrum definieren
+var map = L.map("map").setView([51.96, 7.62], 13);
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-    res.render('poiAnlegen', { title: 'POI Anlegen' });
+// OSM Layer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+// Toolbar zum Zeichnen von Rechtecken
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+var drawControl = new L.Control.Draw({
+  edit: {
+    featureGroup: drawnItems,
+  },
+  draw: {
+    polygon: false,
+    rectangle: false,
+    marker: true,
+    circle: false,
+    polyline: false,
+  },
+});
+map.addControl(drawControl);
+
+// Event Listener, der aktiv wird, wenn fertig gezeichnet wurde
+map.on(L.Draw.Event.CREATED, function (e) {
+  mark = e.layer;
+  map.addLayer(mark);
+
+  coords = e.layer.getLatLng();
+  xInput.value = coords.lat;
+  yInput.value = coords.lng;
+  console.log(coords);
 });
 
-// Wird ausgeführt, wenn der Speichern Button gedrückt wurde
-router.post('/finish', function (req, res, next) {
-    if (req.body.y == null || req.body.y == '' || req.body.x == null || req.body.x == '') {
-        res.send('Keine Koordinaten vorhanden' + '<br><a href="/poiAnlegen">Zurück</a> ');
-    }
-    console.log('A new poi has been added');
-
-    console.log(req.body);
-
-    //geojson
-    let poi = {
-        type: 'Feature',
-        properties: {
-            shape: 'Marker',
-            name: req.body.pname,
-            category: 'default'
-        },
-        geometry: {
-            type: 'Point',
-            coordinates: [req.body.y, req.body.x]
-        }
-    };
-
-    console.log('Parse:');
-    console.log(poi);
-
-    // connect to the mongodb database and afterwards, insert one the new element
-    client.connect(function (err) {
-        console.log('Connected successfully to server');
-
-        const db = client.db(dbName);
-        const collection = db.collection(collectionName);
-
-        // Insert the document in the database
-        collection.insertOne(poi, function (err, result) {
-            console.log(`Inserted ${result.insertedCount} document into the collection`);
-            res.render('add_notification', { title: 'Vorgang abgeschlossen', data: poi });
-        });
-    });
+// Löschen des letzten Punkts
+map.on(L.Draw.Event.DRAWSTART, function (e) {
+  if (mark != null) {
+    map.removeLayer(mark);
+    coords = null;
+    xInput.value = "";
+    yInput.value = "";
+  }
 });
-
-module.exports = router;
